6d39434b8986bb4b82120bd11c627a6eba2b37d7
Pardhiv <pardhiv19nov@gmail.com>
2023-01-26 19:01:31 +0530

    Linked New Node

--- SkipListSearch.cpp
+++ SkipListSearch.cpp
@@ -2,6 +2,8 @@
 #include "SkipListSearch.h"
 using namespace std;
 
+#define MAXI 2147483647
+
 Node::Node()
 {
     this->data = INT_MIN;
@@ -18,6 +20,13 @@ void Node::CreateNode(int data, Node *Back, int cur_level)
     new_temp->ArrNodes[cur_level] = temp;
 }
 
+void Node::LinkNode(Node *Back, Node *New_link, int cur_level)
+{
+    Node *temp = Back->ArrNodes[cur_level];
+    Back->ArrNodes[cur_level] = New_link;
+    New_link->ArrNodes[cur_level] = temp;
+}
+
 void Node::Insert(int giv_data, int level)
 {
     int cur_level = level;
@@ -25,10 +34,10 @@ void Node::Insert(int giv_data, int level)
     Node *cur_node = ArrNodes[size_of_ArrNodes - 1];
     Node *level_node;
     cur_node->data = INT_MIN;
-
+    stack<Node *> level_nodes;
     while (cur_level >= 0)
     {
-        level_node = cur_node;
+        level_nodes.push(cur_node);
         while (giv_data <= cur_node->ArrNodes[cur_level]->data)
         {
             cur_node = cur_node->ArrNodes[cur_level];
@@ -37,4 +46,13 @@ void Node::Insert(int giv_data, int level)
     }
 
     CreateNode(giv_data, cur_node, cur_level);
+    cur_node = cur_node->ArrNodes[cur_level];
+    srand(time(0));
+    int random = rand();
+    cur_level = 1;
+    while (random / (float)MAXI > 0.5)
+    {
+        LinkNode(level_nodes.top(), cur_node, cur_level++);
+        level_nodes.pop();
+    }
 }
--- SkipListSearch.h
+++ SkipListSearch.h
@@ -8,5 +8,6 @@ public:
     int level;
     vector<Node *> ArrNodes;
     void CreateNode(int data, Node *Back, int cur_level);
+    void LinkNode(Node *Back, Node *New_link, int cur_level);
     void Insert(int data, int level);
 };
